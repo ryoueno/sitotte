036cd14bc26c134288026f02af3ba5c0739c0ec9
uenoryo <r.r.ag0611@outlook.jp>
2016-11-11 10:35:36 +0900

    ユーザ認証実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  def index
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  get '/users', to: 'users#index'
+
+  get 'users/new'
+
+  devise_for :users
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
