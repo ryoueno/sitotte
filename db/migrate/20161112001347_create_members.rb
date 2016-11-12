803fb7356c464bfc534883dca26a2971f39d343b
uenoryo <r.r.ag0611@outlook.jp>
2016-11-12 09:31:53 +0900

    member tableをnot null

--- db/migrate/20161112001347_create_members.rb
+++ db/migrate/20161112001347_create_members.rb
@@ -1,8 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-      t.integer :group_id
-      t.integer :user_id
+      t.integer :group_id, :null => false
+      t.integer :user_id, :null => false
       t.boolean :is_admin, :null => false, :default => false
 
       t.timestamps
