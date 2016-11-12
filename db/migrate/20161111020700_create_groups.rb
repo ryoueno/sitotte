82a6036b07dc326655f62cc15d3524a19ad77a22
uenoryo <r.r.ag0611@outlook.jp>
2016-11-12 09:20:32 +0900

    groups table からadmin_id 除去

--- db/migrate/20161111020700_create_groups.rb
+++ db/migrate/20161111020700_create_groups.rb
@@ -1,7 +1,6 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.integer :admin_id, :null => false
       t.string :name
       t.integer :limit
 
