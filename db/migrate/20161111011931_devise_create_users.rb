6196e48db3fbb2c8a12a6bd530afe5d68ef694bb
uenoryo <r.r.ag0611@outlook.jp>
2016-11-14 12:28:43 +0900

    userにis_guest追加

--- db/migrate/20161111011931_devise_create_users.rb
+++ db/migrate/20161111011931_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :email,              null: false, default: ""
       t.string :name,               null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :is_guest,          null: false, default: 0
 
       ## Recoverable
       t.string   :reset_password_token
