f51a3b1a29fa276c6a4ea2ead3a106cb1cf3e859
uenoryo <r.r.ag0611@outlook.jp>
2016-11-14 21:28:19 +0900

    Seeder 追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    User.create(:name => 'uenoryo', :email => 'a@a.com', :password => 'aaaaaa')
+    User.create(:name => 'uenoryo2', :email => 'a2@a.com', :password => 'aaaaaa')
+    User.create(:name => 'uenoryo3', :email => 'a3@a.com', :password => 'aaaaaa')
