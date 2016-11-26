0cf015114213c3c5498bf4e0092e9a1697891508
uenoryo <r.r.ag0611@outlook.jp>
2016-11-26 11:36:23 +0900

    ticketBoxを改造

--- app/assets/javascripts/components/ticket_box.js.jsx
+++ app/assets/javascripts/components/ticket_box.js.jsx
@@ -1,13 +1,18 @@
-var TicketBox = React.createClass({
-  loadTicketsFromServer: function() {
+var TicketBoxList = React.createClass({
+  showModalForm(e) {
+    $('#modal_box').modal('show');
+  },
+  loadTicketsFromServer: function(id) {
     $.ajax({
-      url: this.props.url,
+      url: this.props.url + id,
       dataType: 'json',
       success: function(result) {
-        this.setState({data: result.data});
+        var state = [];
+        state['data' + id] = result.data;
+        this.setState(state);
       }.bind(this),
       error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+
       }.bind(this)
     });
   },
@@ -30,41 +35,60 @@ var TicketBox = React.createClass({
     $('#success-alert').fadeIn(500).delay(2000).fadeOut(500);
   },
   getInitialState: function() {
-    return {
-      data: [],
-      member_id: this.props.member_id
-    };
+    // data3 = [data] のように data+idをkeyとしてstateを初期化
+    var data = new Object;
+    for (var i in this.props.members_id) {
+      data['data' + this.props.members_id[i]] = [];
+    }
+    data['data' + this.props.current_member_id] = [];
+    return data;
   },
-  componentDidMount: function() {
-    this.loadTicketsFromServer();
-    //pollIntervalおきにデータ更新
-    //setInterval(this.loadTicketsFromServer, this.props.pollInterval);
-  },
-  //TicketFrom送信時に親(TicketBox)の持つstate更新用のコールバック関数を渡しておく
   render: function() {
-    return (
-      <div className="ticketBox">
+    var ticket_list = [];
+    ticket_list.push(
+      <div key={this.props.current_member_id} className="tab-pane active" id={"user-tab" + this.props.current_member_id}>
+        <TicketList loadTickets={this.loadTicketsFromServer} data={this.state['data' + this.props.current_member_id]} member_id={this.props.current_member_id} />
+      </div>
+    );
+    for (var i in this.props.members_id) {
+      ticket_list.push(
+        <div key={this.props.members_id[i]} className="tab-pane" id={"user-tab" + this.props.members_id[i]}>
+          <TicketList key={this.props.members_id[i]} loadTickets={this.loadTicketsFromServer} data={this.state['data' + this.props.members_id[i]]} member_id={this.props.members_id[i]} />
+        </div>
+      );
+    }
+    return(
+      <div>
+        <div className="click_btn" onClick={this.showModalForm}>
+          作成
+        </div>
         <div className="modal fade" id="modal_box">
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="box_inner">
-                <TicketForm onTicketSubmit={this.handleTicketSubmit} member_id={this.state.member_id}/>
+                <TicketForm key={this.props.current_member_id} onTicketSubmit={this.handleTicketSubmit} member_id={this.props.current_member_id}/>
                 <p className="text-center"><a className="btn btn-primary" data-dismiss="modal" href="#" id="close-btn">閉じる</a></p>
               </div>
             </div>
           </div>
         </div>
-        <TicketList data={this.state.data} />
+        <div className="tab-content">
+          {ticket_list}
+        </div>
       </div>
     );
   }
 });
 
 var TicketList = React.createClass({
+  componentDidMount: function() {
+    //最初のrender時に更新
+    this.props.loadTickets(this.props.member_id);
+  },
   render: function() {
     var ticketNodes = this.props.data.map(function (ticket) {
       return (
-        <Ticket title={ticket.title}>
+        <Ticket key={ticket.id} title={ticket.title}>
           {ticket.body}
         </Ticket>
       );
@@ -96,19 +120,14 @@ var TicketForm = React.createClass({
     ReactDOM.findDOMNode(this.refs.state_id).value = '';
     return;
   },
-  getInitialState() {
-    return {
-      member_id: this.props.member_id
-    };
-  },
   render: function() {
     return (
-      <form className="ticketForm" onSubmit={this.handleSubmit}>
+      <form className="ticketForm" onSubmit={this.props.onTicketSubmit}>
         <input type="text" placeholder="タイトル" ref="title" />
         <input type="text" placeholder="本文" ref="body" />
         <input type="number" placeholder="ユーザID" ref="assign_to" />
         <input type="number" placeholder="ステータス" ref="state_id" />
-        <input type="hidden" value={this.state.member_id} ref="created_by" />
+        <input type="hidden" value={this.props.member_id} ref="created_by" />
         <input type="submit" value="Post" className="btn btn-primary" />
       </form>
     );
