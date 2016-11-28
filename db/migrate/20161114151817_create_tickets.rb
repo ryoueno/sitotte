58aba4a9d7b1291d21b63d23e208f64f8201f70c
uenoryo <r.r.ag0611@outlook.jp>
2016-11-28 22:19:17 +0900

    Change field values null to not null

--- db/migrate/20161114151817_create_tickets.rb
+++ db/migrate/20161114151817_create_tickets.rb
@@ -1,13 +1,13 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
-      t.integer :assign_to
-      t.string :title
-      t.text :body
-      t.integer :state_id, :default => 1
-      t.integer :priority_id, :default => 1
-      t.date :deadline, :null => true
-      t.integer :created_by
+      t.integer :assign_to, :null => false
+      t.string :title, :null => false
+      t.text :body, :null => false
+      t.integer :state_id, :null => false, :default => 1
+      t.integer :priority_id, :null => false, :default => 1
+      t.date :deadline
+      t.integer :created_by, :null => false
 
       t.timestamps
     end
